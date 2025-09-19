32418cfb449e44eb4028527c1bd547bdeb3974ea
Vairav Lakshmanan <vairavmahi@gmail.com>
2025-09-19 08:05:34 +0530

    Remove hardcoding credentials as app is hosted in amplify

--- lib/dynamo.ts
+++ lib/dynamo.ts
@@ -3,10 +3,6 @@ import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb"
 
 const client = new DynamoDBClient({
   region: process.env.NEXT_PUBLIC_REGION || "us-east-1",
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
-  },
   maxAttempts: 3,
   requestHandler: undefined,
 })
